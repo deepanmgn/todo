612c2e47fa7cb07e084818c66aff4d5e282a99ba
Deepan Muruan <deep.mgn@gmail.com>
2016-02-01 13:57:24 +0530

    Installed bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
